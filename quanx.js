a1996e4815e4ada8ed4eb24f21733d06424b6fd5
phinexdaz <phinexdaz@gmail.com>
2019-11-03 11:28:13 +0800

    Update quanx.js

--- quanx.js
+++ quanx.js
@@ -1,7 +1,7 @@
-/**
- * @supported 8FFA6E55 60ED4873
- */
+/*
+@supported 8FFA6E55 60ED4873
+*/
 var body = $response.body;
-body = '\/**\n * @supported 8FFA6E55 60ED4873\n*\/\n' + body;
+body = '\/*\n@supported 8FFA6E55 60ED4873\n*\/\n' + body;
 
 $done(body);
